2c603406387f728575ea61a38c3666105c32ad30
Iskz17 <61486047+Iskz17@users.noreply.github.com>
2020-10-31 09:40:03 +0800

    experimenting on the svg a little

--- homeeztest/src/components/Layout.jsx
+++ homeeztest/src/components/Layout.jsx
@@ -8,18 +8,7 @@ class Layout extends Component {
   }
   render() {
     return (
-      <svg
-      width="100"
-      height="100"
-        id={this.props.data.id}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-      >
-        {/* <g fill="#61DAFB"> */}
-          <SvgParameter data={this.props.data} />
-          {console.log(this.props.data.tag)}
-        {/* </g> */}
-      </svg>
+          <SvgParameter data={this.props.data} />    
     );
   }
 }
